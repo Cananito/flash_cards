e070ff0061e8baf9a68da26050913a43c7d70f2e
Rogelio Gudino <cananito@gmail.com>
2025-02-01 21:49:23 -0800

    Pipes fastcgi variables into requests and tries a couple of them out.

--- backend/main.c
+++ backend/main.c
@@ -1,4 +1,6 @@
 #include <fcgiapp.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
   FCGX_Init();
@@ -6,7 +8,12 @@ int main() {
   FCGX_Request request;
   FCGX_InitRequest(&request, socket, 0);
   while(FCGX_Accept_r(&request) >= 0) {
-    FCGX_FPrintF(request.out, "Accept\nContent-type: text/html\r\n\r\nSynco de Mayo!\n");
+    FCGX_FPrintF(request.out,
+                 "Accept\nContent-type: text/html\r\n\r\nSynco de Mayo!\n");
+    char* request_uri = FCGX_GetParam("REQUEST_URI", request.envp);
+    printf(">>> Request URI: %s\n", request_uri);
+    char* query_string = FCGX_GetParam("QUERY_STRING", request.envp);
+    printf(">>> Query string: %s\n", query_string);
     FCGX_Finish_r(&request);
   }
   return 0;
